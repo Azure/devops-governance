61e727051b72fc1397832098b068e47c081fa3e1
Julie Ng <me@julie.io>
2020-09-07 08:34:06 +0200

    tf(outputs): remove service principal secrets

--- modules/workspace/outputs.tf
+++ modules/workspace/outputs.tf
@@ -1,26 +1,26 @@
-output "workspace" {
-  value = {
-    resource_group_name	= azurerm_resource_group.workspace.name
-    storage_account     = azurerm_storage_account.storage.name
-    key_vault           = azurerm_key_vault.kv.name
-  }
+output "resource_group_name" {
+  value = azurerm_resource_group.workspace.name
 }
 
-output "workspace_service_principal" {
-  value =  {
-    display_name   = azuread_application.workspace_sp.name
-    object_id      = azuread_application.workspace_sp.object_id
-    application_id = azuread_application.workspace_sp.application_id
-    client_secret  = random_password.workspace_sp.result
-  }
+output "storage_account" {
+  value = azurerm_storage_account.storage.name
 }
 
+output "key_vault" {
+  value = azurerm_key_vault.kv.name
+}
 
-output "key_vault_reader_service_principal" {
-  value =  {
-    display_name   = azuread_application.kv_reader_sp.name
-    object_id      = azuread_application.kv_reader_sp.object_id
-    application_id = azuread_application.kv_reader_sp.application_id
-    client_secret  = random_password.kv_reader_sp.result
-  }
+output "service_principals" {
+  value =  [
+    {
+      display_name   = azuread_application.workspace_sp.name
+      object_id      = azuread_application.workspace_sp.object_id
+      application_id = azuread_application.workspace_sp.application_id
+    },
+    {
+      display_name   = azuread_application.kv_reader_sp.name
+      object_id      = azuread_application.kv_reader_sp.object_id
+      application_id = azuread_application.kv_reader_sp.application_id
+    }
+  ]
 }
